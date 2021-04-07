a47a62fcd325fc29305c46606758877ab53ae2f5
smileysakai <senkuhatoki@hotmail.com>
2021-04-07 11:57:16 +1000

    part3a3.7-3.8 submission

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
 
 app.use(express.json())
+
+//app.use(morgan('tiny'))
+
+morgan.token('value', function (req, res) { return JSON.stringify(req.body) })
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :value'))
 
 let persons = [  
     {
