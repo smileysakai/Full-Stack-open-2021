e3782351f942f9bcd07584bdf33013b26d03b75e
smileysakai <senkuhatoki@hotmail.com>
2021-04-15 14:06:08 +1000

    part3-3.15-3.18 submission

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -17,6 +17,9 @@ morgan.token('value', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :value'))
 
+require('dotenv').config()
+const Person = require('./models/people')
+
 let persons = [  
     {
         "name": "Dan Abramov",
@@ -46,7 +49,9 @@ let persons = [
 ]
     
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(people => {
+        response.json(people)
+    })
 })
 
 app.get('/info', (request, response) => {
@@ -55,56 +60,94 @@ app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${personL} people <br/><br/> ${date}`)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id===id)
-    if (person){
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(result => {
+            response.json(result)
+        })
+        .catch(error => next(error))
+
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
+app.delete('/api/persons/:id', (request, response, next) => {
+    /*const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
     response.status(204).end()
+    */
+   Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
+/*
 const generateId = () => {
     const maxId = persons.length > 0
       ? Math.max(...persons.map(person => person.id))
       : 0
     return maxId + 1
   }
-  
+  */
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    const person = {
+      number: body.number,
+    }
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+})
+
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
-    if (!body.name || !body.number) {
+    if (body.name === undefined || body.number === undefined) {
         return response.status(400).json({ 
         error: 'name or number missing' 
         })
     }
-
+/*
     if (persons.find(person => person.name === body.name)){
         return response.status(400).json({ 
             error: `name must be unique` 
         })
     }
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: generateId(),
-    }
+    */
+   Person.find().byName(body.name).exec((err, people) => {
+        const [personquery] = people
+        if (personquery === undefined){
+            const person = new Person({
+                name: body.name,
+                number: body.number,
+            })
 
-    persons = persons.concat(person)
-
-    response.json(persons)
+            person.save().then(savedPerson => {
+                response.json(savedPerson)
+            }) 
+        }
+    })
 })
 
 
-const PORT = process.env.PORT || 3001
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+  // this has to be the last loaded middleware.
+  app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ part3/phonebookbackend/mongo.js
@@ -0,0 +1,45 @@
+const e = require('cors')
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.lvkji.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+  
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+  console.log('phonebook:')
+  Person.find({}).then( persons => {
+    persons.forEach( person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+    process.exit(1)
+  })
+} else if (process.argv.length === 5) {
+  const name = process.argv[3]
+  const number = String(process.argv[4])
+
+  const contact = new Person ({
+      name: name,
+      number: number
+  })
+
+  contact.save().then( result => {
+      console.log(`added ${name} number ${number} to phonebook`)
+      mongoose.connection.close()
+  })
+}
